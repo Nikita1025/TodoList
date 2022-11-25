d7df5630372775a60f32f31a59667e031e9a4bfb
Nikita1025 <nikita1025@mail.ru>
2022-11-25 15:16:18 +0300

    using useCallback and memo

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import React, {useCallback} from 'react';
+import './App.css';
+import {AddItemForm} from './AddItemForm';
+import {useDispatch, useSelector} from 'react-redux';
+import {AppRootStateType} from './state/store';
+import {AppBar, Button, Container, Grid, IconButton, Paper, Toolbar, Typography} from "@mui/material";
+import {Menu} from "@mui/icons-material";
+import {addTodolistAC} from "./state/todolist-reducer";
+import {TaskType, Todolist} from "./Todolist";
+
+export type FilterValuesType = "all" | "active" | "completed";
+export type TodolistType = {
+    id: string
+    title: string
+    filter: FilterValuesType
+}
+
+export type TasksStateType = {
+    [key: string]: Array<TaskType>
+}
+
+
+function App() {
+    const todolists = useSelector<AppRootStateType, Array<TodolistType>>(state => state.todolists)
+    const dispatch = useDispatch();
+    const addTodolist=useCallback((title: string)=>{
+        const action = addTodolistAC(title);
+        dispatch(action);
+    },[dispatch])
+
+    return (
+        <div className="App">
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <Menu/>
+                    </IconButton>
+                    <Typography variant="h6">
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+            <Container fixed>
+                <Grid container style={{padding: "20px"}}>
+                    <AddItemForm addItem={addTodolist}/>
+                </Grid>
+                <Grid container spacing={3}>
+                    {
+                        todolists.map(tl => {
+                            return <Grid item key={tl.id}>
+                                <Paper style={{padding: "10px"}}>
+                                    <Todolist
+                                       key={tl.id}
+                                       todolist={tl}
+                                    />
+                                </Paper>
+                            </Grid>
+                        })
+                    }
+                </Grid>
+            </Container>
+        </div>
+    );
+}
+
+export default App;
