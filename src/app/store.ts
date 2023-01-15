4a3fa95667e84166ab2b1e6a3f49a4ce963ef6fc
Nikita1025 <nikita1025@mail.ru>
2023-01-15 14:13:18 +0300

    correct code

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,8 @@
 import {tasksReducer} from '../features/TodolistsList/tasks-reducer';
 
-import {AnyAction, applyMiddleware, combineReducers, legacy_createStore} from 'redux'
+import {AnyAction, combineReducers,} from 'redux'
 import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
-import thunkMiddleware, {ThunkDispatch} from 'redux-thunk'
+import thunkMiddleware, {ThunkAction, ThunkDispatch} from 'redux-thunk'
 import {appReducer} from './app-reducer'
 
 import {configureStore} from "@reduxjs/toolkit";
@@ -10,27 +10,21 @@ import {authReducer} from "../features/Login/auth-reducer";
 import {todolistsReducer} from "../features/TodolistsList/todolists-reducer";
 
 
-// объединяя reducer-ы с помощью combineReducers,
-// мы задаём структуру нашего единственного объекта-состояния
 const rootReducer = combineReducers({
     tasks: tasksReducer,
     todolists: todolistsReducer,
     app: appReducer,
     auth: authReducer
 })
-// непосредственно создаём store
-//export const store = legacy_createStore(rootReducer, applyMiddleware(thunkMiddleware));
 export const store = configureStore({
     reducer: rootReducer,
     middleware: (getDefaultMiddleware) =>getDefaultMiddleware().prepend(thunkMiddleware)
 })
 
-// определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
-// создаем тип диспатча который принимает как AC так и TC
-export type AppThunkDispatch = ThunkDispatch<AppRootStateType, any, AnyAction>
-
-export const useAppDispatch = () => useDispatch<AppThunkDispatch>();
+export type ThunkDispatchType = ThunkDispatch<AppRootStateType, any, AnyAction>
+export type AppThunkType<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, AnyAction>
+export const useAppDispatch = () => useDispatch<ThunkDispatchType>();
 export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
 
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
