59eae8d6dcbd6fc040349b98104560a56c919bc4
Nikita1025 <nekit1025l@gmail.com>
2023-02-08 12:22:14 +0300

    Updates

---
+++ precache-manifest.b56a2c3c617f6a41bd5b675eaee80dd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "674c2f6995add590272449cc1d1c33bf",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "92716fcd7b4839090d74",
+    "url": "/TodoList/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "eedc512af3c91e6cc62b",
+    "url": "/TodoList/static/js/2.317bd65b.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/TodoList/static/js/2.317bd65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92716fcd7b4839090d74",
+    "url": "/TodoList/static/js/main.1b6d95fd.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
